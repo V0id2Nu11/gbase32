90d0c21e3934fbd83ad91446395eed7c45dd22e4
Z0main <zsp.main.zsp+github@gmail.com>
2020-07-05 20:20:25 +0800

    feat: cli

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include "gbase32.h"
+#include "config.h"
 #include <stdio.h>
 #include <glib.h>
 
+
+static gboolean verbose = FALSE;
+static gchar* text2decode = NULL;
+static gboolean show_hex = FALSE;
+
+static GOptionEntry entries[] = {
+    {"decode", 'd', 0, G_OPTION_ARG_STRING, &text2decode,
+        "Decode <text> to data", NULL},
+    {"hex", 'h', 0, G_OPTION_ARG_NONE, &show_hex,
+        "Show decode result in hex", NULL},
+    {"verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose,
+        "Be verbose", NULL},
+    {NULL},
+};
 
 guchar* 
 decode2string(
@@ -9,26 +24,6 @@ decode2string(
         gsize* out_len)
 {
     return g_base32_decode(text2decode, out_len);
-}
-
-void
-printdecode2hex(
-        guchar* text2decode,
-        gsize* out_len)
-{
-    guchar* str_data = g_base32_decode(text2decode, out_len);
-    gsize str_len = strlen(str_data);
-
-    guchar* str_start;
-    str_start = str_data;
-    guchar* str_end = str_data + str_len;
-
-    int i = 0;
-
-    while (str_start < str_end) {
-        g_print("[%d: %#x]", i++, *str_start++);
-    }
-    g_print("\n");
 }
 
 guchar*
@@ -62,24 +57,30 @@ main(
         int argc,
         char** argv)
 {
-    if (argc <= 1) {
-        printf("too less arg\n");
-        return -1;
+    GError *error = NULL;
+    GOptionContext* context;
+
+    context = g_option_context_new("- base32 encode & decode");
+    g_option_context_add_main_entries(context, entries, NULL);
+    g_option_context_set_summary(context, "GBase32 encode & decode");
+
+    if (!g_option_context_parse(context, &argc, &argv, &error)) {
+        g_print("option parsing failed: %s\n", error->message);
+        exit(1);
     }
 
-    unsigned char* text2decode = argv[1];
+    if (text2decode != NULL) {
+        gsize out_len = 0;
+        if (show_hex) {
+            g_print("%s\n", decode2hex(text2decode, &out_len));
+        } else {
+            g_print("%s\n", decode2string(text2decode, &out_len));
+        }
+        g_free(text2decode);
+    }
 
-    long unsigned int out_len;
+    g_option_context_free(context);
 
-    unsigned char* decoded_text = decode2string(text2decode, &out_len);
-    printf("decode text: %s\n", decoded_text);
-    printf("decode text len: %d\n", out_len);
-
-    printf("====>\n");
-    printdecode2hex(text2decode, &out_len);
-
-    printf("====>\n");
-    unsigned char* decoded_hex = decode2hex(text2decode, &out_len);
-    printf("hex: %s\n", decoded_hex);
+    return 0;
 }
 
